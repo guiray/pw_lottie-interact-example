24139cb67badac2ef9c6201cd11a31fbf8dea1b6
guiray <guiray@users.noreply.github.com>
2023-09-15 12:04:20 -0400

    Trying to add interactivity. not working for now

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,6 @@
 import Lottie from './Lottie';
+import LottiePlayer from './LottiePlayer';
+import { LottieInteractivity } from '@lottiefiles/lottie-interactivity';
 
 class Main {
   constructor() {
@@ -6,8 +8,9 @@ class Main {
   }
 
   init() {
-
     new Lottie();
+    new LottiePlayer();
+    new LottieInteractivity();
   }
 }
 new Main();
